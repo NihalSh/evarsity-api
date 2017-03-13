0f8e80efe72443217772f955a8718ef3b9e422d2
NihalSh <nihal.sh.1995@gmail.com>
2017-03-13 19:36:21 +0530

    grades parser

---
+++ handlers/grades.js
@@ -0,0 +1,50 @@
+"use strict";
+const cheerio = require('cheerio')
+const querystring = require('querystring')
+const request = require('request-promise-native')
+
+module.exports = (req, res, next) => {
+	if(req.params.id){
+		let options = {
+			uri: 'http://5@evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=6',
+			headers: {
+				Cookie: `JSESSIONID=${querystring.unescape(req.params.id)}`
+			}
+		}
+		request(options)
+			.then((response) => {
+				let json = parser(response)
+				if (json !== null) {
+					res.setHeader('Content-Type', 'application/json');
+					res.send(json)
+				} else {
+					res.sendStatus(401)
+				}
+			})
+			.catch((err) => {
+				next(err)
+			})
+	}else{
+		req.log.info("token absent")
+		next()
+	}
+}
+
+function parser(body) {
+	let $ = cheerio.load(body)
+	let gpa = []
+	$('td[colspan="2"]').each(function(index, element) {
+			gpa.push(+$(element).text().trim())
+		}
+	)
+	if (gpa.length === 0) {
+		return null
+	} else {
+		let result = {}
+		result.cgpa = gpa[gpa.length - 1]
+		for (let i = 0; i < gpa.length - 1; i++) {
+			result[`sgpa${i + 1}`] = gpa[i]
+		}
+		return result
+	}
+}
--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 "use strict"
+const grades = require('./handlers/grades.js')
 const login = require('./handlers/login.js')
 const profile = require('./handlers/profile.js')
 
 module.exports = (app) => {
 	app.get('/', login)
 	app.get('/profile/:id', profile)
+	app.get('/grades/:id', grades)
 }
