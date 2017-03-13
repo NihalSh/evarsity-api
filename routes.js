06cfd94bdc6e8adce92d3db96aeef083f67f8ecd
NihalSh <nihal.sh.1995@gmail.com>
2017-03-13 19:11:53 +0530

    general profile

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 "use strict"
 const login = require('./handlers/login.js')
+const profile = require('./handlers/profile.js')
 
 module.exports = (app) => {
 	app.get('/', login)
+	app.get('/profile/:id', profile)
 }
