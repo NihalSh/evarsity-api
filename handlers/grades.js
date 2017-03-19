dd6749489b8b217ddd40e339a2cfa3ec59165510
NihalSh <nihal.sh.1995@gmail.com>
2017-03-19 16:14:52 +0530

    arrears and subject information

--- handlers/grades.js
+++ handlers/grades.js
@@ -45,6 +45,57 @@ function parser(body) {
 		for (let i = 0; i < gpa.length - 1; i++) {
 			result[`sgpa${i + 1}`] = gpa[i]
 		}
+
+		/*subjects*/
+		let table = $('table').first()
+		let overall = []
+		let semester = []
+		let subject = {}
+		let headings = []
+		table.find('tr[class="subTitle"]').find('td').each(function(index, element) {
+			headings.push($(element).text().trim())
+		})
+		table.find('tr').each(function(index, element) {
+			if ($(element).attr('align') === "left") {
+				$(element).find('td').each(function(index, element) {
+					subject[headings[index]] = $(element).text().trim()
+				})
+				semester.push(subject)
+				subject = {}
+			} else {
+				if ( semester.length !== 0) {
+					overall.push(semester)
+					semester = []
+				}
+			}
+		})
+
+		for (let i = 0; i < overall.length; i++) {
+			result[`sem${i + 1}`] = overall[i]
+		}
+		let subjects = {}
+		for (let semester of overall) {
+			for (let subject of semester) {
+				if (!subjects[subject.Code]) {
+					subjects[subject.Code] = new Set()
+				}
+				subjects[subject.Code].add(subject.Grade)
+			}
+		}
+		let arrears = false
+		let i = null
+		for (let code of Object.keys(subjects)) {
+			i = subjects[code]
+			if (i.has('S') || i.has('A') || i.has('B') || i.has('C') || i.has('D')) {
+				delete subjects[code]
+			}
+			if (!arrears && (i.has('W') || i.has('U') || i.has('I') || i.has('*'))) {
+				arrears = true
+			}
+		}
+
+		result['historyArrears'] = arrears
+		result['standingArrears'] = Object.keys(subjects).length
 		return result
 	}
 }
