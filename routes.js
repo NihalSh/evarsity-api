a3a07ec0cac2794e0e628a82bae654f2acc8dd25
NihalSh <nihal.sh.1995@gmail.com>
2017-03-29 23:03:13 +0530

    attendance

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 "use strict"
+const attendance = require('./handlers/attendance.js')
 const grades = require('./handlers/grades.js')
 const login = require('./handlers/login.js')
 const profile = require('./handlers/profile.js')
@@ -7,4 +8,5 @@ module.exports = (app) => {
 	app.get('/', login)
 	app.get('/profile/:id', profile)
 	app.get('/grades/:id', grades)
+	app.get('/attendance/:id', attendance)
 }
